b57463283c150964eb18ede39b274d9c4ff7446e
Elijah Kamanga <elijahkamanga1@gmail.com>
2025-08-29 22:20:14 +0300

    cleared the not showing movie errors

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -5,14 +5,14 @@ import {useParams} from 'react-router-dom'
 function Movie() {
   
   const{id}=useParams()
-  const[movie,setMovie]=useState([])
+  const[movie,setMovie]=useState(null)
 
   useEffect(()=>{
-    fetch('http://localhost:4000/movies/${id}')
+    fetch(`http://localhost:4000/movies/${id}`)
     .then((res)=>res.json())
     .then((data)=>setMovie(data))
     .catch((err)=>console.error('Error fetching movies:',err))
-  },[])
+  },[id])
 
   if(!movie)
     return(
@@ -21,7 +21,7 @@ function Movie() {
   return (
     <>
    <h1>{movie.title}</h1>
-   <p>Time:{movie.item}minutes</p>
+   <p>Time:{movie.time}minutes</p>
    <ul>
     {movie.genres.map((genre,index)=>(
       <li key={index}>{genre}</li>
